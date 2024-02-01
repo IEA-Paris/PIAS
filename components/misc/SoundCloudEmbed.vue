d705fd4dbdb43703dc854be5e3325d17ca73a11b
CI <username@users.noreply.github.com>
2024-02-01 15:16:28 +0100

    feat: added soundclound player

---
+++ components/misc/SoundCloudEmbed.vue
@@ -0,0 +1,32 @@
+<template>
+  <iframe
+    title="SoundCloud Embed"
+    width="100%"
+    height="166"
+    scrolling="no"
+    frameborder="no"
+    allow="autoplay"
+    :src="
+      'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' +
+      src +
+      '&color=%235c3190&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true'
+    "
+  ></iframe>
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+}
+</script>
