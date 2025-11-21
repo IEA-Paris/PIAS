c219cb5565d2203f3ca350fe4f318c35317fc56f
billybobbonnet <boitalettre9@gmail.com>
2025-11-21 09:34:56 +0100

    fix: trailing slash breaking page change

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,9 +55,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   ssr: true,
-  router: {
-    trailingSlash: true, // Enforce trailing slashes to avoid redirects
-  },
   generate: {
     dir: 'dist',
     fallback: true,
